850a844c7055edea3fe9ebdf89dae82b50288c96
Donnovan-Machado <donnovanriddle19@gmail.com>
2021-12-02 19:20:10 -0300

    aula 3

---
+++ pages/utilitario.ts
@@ -0,0 +1,14 @@
+import { Menu } from "../classes/menu.js";
+
+
+
+export function obterMenu() {
+    let menuLateral: Array<Menu> = new Array<Menu>()
+    menuLateral.push(new Menu("Home", '/pages/principal.html', true))
+    menuLateral.push(new Menu("Cadastro de usuarios", '/pages/usuarios/cadastrar.html', true))
+   // menuLateral.push(new Menu("Cadastro de alunos", '/pages/alunos/cadastrar_alunos.html', false))
+   // menuLateral.push(new Menu("Oliota.com", 'https://oliota.com/', false))
+    return menuLateral
+
+
+}
